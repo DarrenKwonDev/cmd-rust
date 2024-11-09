4c0a553386603e262ccccb2d6db453d66d7b1836
DarrenKwonDev <darrenkwondev46@gmail.com>
2024-11-09 19:39:12 +0900

    echor

---
+++ echor/src/main.rs
@@ -0,0 +1,22 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about)]
+// this comment will be 'about'
+struct Args {
+    #[arg(required(true))]
+    text: Vec<String>,
+
+    #[arg(short('n'))]
+    omit_newline: bool,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    print!(
+        "{}{}",
+        args.text.join(" "),
+        if args.omit_newline { "" } else { "\n" }
+    );
+}
