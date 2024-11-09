b3d11e95158945bf682573052dd097eaa31f3283
DarrenKwonDev <darrenkwondev46@gmail.com>
2024-11-09 15:55:58 +0900

    hello:

---
+++ hello/tests/cli.rs
@@ -0,0 +1,17 @@
+// use std::process::Command;
+use assert_cmd::{Command};
+use pretty_assertions::assert_eq;
+
+#[test]
+fn works() {
+    assert!(true);
+}
+
+#[test]
+fn runs() {
+    let mut cmd = Command::cargo_bin("hello").unwrap();
+    let output= cmd.output().expect("fail");
+    assert!(output.status.success());
+    let stdout = String::from_utf8(output.stdout).expect("invalid UTF-8");
+    assert_eq!(stdout, "Hello, world!\n");
+}
